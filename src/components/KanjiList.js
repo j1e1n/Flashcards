8e9acab8a66bd4520e932ff258f081ee8882db62
Jennifer Wardhaugh <jenwardhaugh@outlook.com>
2021-06-15 11:44:33 +0100

    grade one filter added

--- src/components/KanjiList.js
+++ src/components/KanjiList.js
@@ -9,16 +9,64 @@ const KanjiList = ({allKanji}) =>  {
         )
     }
 
-    const kanjiListItems = allKanji.map((character, index) => {
-        return <li key={index}>{character.kanji.character}</li>
-    })
+    
+    const gradeOneList = allKanji.filter(kanji => kanji.references.grade === 1)
+        .map((filteredKanji, index) => {
+            return <li key={index}>{filteredKanji.kanji.character}</li>
+        })
+        
+    console.log("gradeOne", gradeOneList)
+    
+    // const kanjiListItems = allKanji.map((character, index) => {
+        //     return <li key={index}>{character.kanji.character}</li>
+        // })
+
+
+
+    // 1. select grade (1-5?)
+
+    // 2. save selection as variable
+
+    // use switch statement to execute filtering of chosen grade
+    // switch(selectedGrade) {
+    //     case 'grade 1':
+    //         listItems = gradeOneList;
+    //         break;
+    //     case 'grade 2':
+    //         listItems = gradeTwoList;
+    //         break;
+    //     case 'grade 3':
+    //         listItems = gradeThreeList;
+    //         break;
+    //     case 'grade 4':
+    //         listItems = gradeFourList;
+    //         break;
+    //     case 'grade 5':
+    //         listItems = gradeFiveList;
+    //         break;
+        
+    // }
+
+
+    // 4. display filtered list
+
+
+
+
+
+
+
+
+
+
+
 
 
 
     return(
         <>
         <ol>
-            {kanjiListItems}
+            {gradeOneList}
         </ol>
         </>
     )
